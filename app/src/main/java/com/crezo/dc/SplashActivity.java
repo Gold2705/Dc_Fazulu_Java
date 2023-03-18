b178ef744a49e55afbfd66b510457ed7dbb03bb9
thangapandi <thangapandi275@gmail.com>
2023-03-19 01:31:00 +0530

    common changes are done

--- app/src/main/java/com/crezo/dc/SplashActivity.java
+++ app/src/main/java/com/crezo/dc/SplashActivity.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-@SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
 
     Handler handler;
